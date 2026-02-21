bd7e7e6729c8f8d5e0510c89e113d7d3b9923fe6
Craig <spamwall@charter.net>
2026-02-21 10:42:33 -0600

    Refactored

--- src/main/java/com/nozzle/optimization/MonteCarloUncertainty.java
+++ src/main/java/com/nozzle/optimization/MonteCarloUncertainty.java
@@ -9,7 +9,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.function.Function;
-import java.util.stream.DoubleStream;
 
 /**
  * Performs Monte Carlo uncertainty propagation analysis for nozzle design.
